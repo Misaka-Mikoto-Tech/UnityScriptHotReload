af545fd617f5f17e2a28187deabd075ce7309eaa
Misaka Mikoto <easy66@live.com>
2022-11-29 17:39:10 +0800

    * 上传测试场景需要的 TextMesh Pro 资源

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,13 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
+    public Button btnTest;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        btnTest.onClick.AddListener(OnBtnTest);
+        NS_Test.Test3.s_val = 456;
+    }
+
+    void OnBtnTest()
+    {
+        Debug.Log($"Test3.s_val={NS_Test.Test3.s_val}");
+
+        var test = new NS_Test.Test3();
+        test.Test(out int val);
+        Debug.Log($"OnBtnTest:val={val}");
+
+        Debug.Log($"Test3.s_val={NS_Test.Test3.s_val}");
     }
 
     // Update is called once per frame
@@ -22,6 +37,8 @@ namespace NS_Test
 {
     public class Test3
     {
+        public static int s_val = 123;
+
         public Action<int> act = x =>
         {
             x += 2;
@@ -30,9 +47,16 @@ namespace NS_Test
 
         public void Test(out int val)
         {
-            val = 2;
+            val = 2000;
             Func<int, bool> f = (int x) => { Debug.Log($"{x + 1}..."); return x > 101; };
-            Debug.Log($"x is OK:{f(val + 2)}");
+            Debug.Log($"<color=yellow>x is OK:{f(val + 2)}</color>");
+            Test2();
+        }
+
+        public void Test2()
+        {
+            string assPath = new Action(Test2).Method.DeclaringType.Assembly.Location;
+            Debug.Log($"location of current dll:{assPath}");
         }
 
         public string TestG<T>(out int val1, T val2) where T : UnityEngine.Object
